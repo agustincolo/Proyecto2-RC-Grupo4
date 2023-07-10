9bd9909c9d70c29deca069a560c1dfbc26a08928
Agustin Nicolas Colodny <agustincolodny@gmail.com>
2023-07-09 23:29:58 -0300

    admin finish

--- js/administration.js
+++ js/administration.js
@@ -7,9 +7,11 @@ const name_id = document.getElementById("name_id")
 const category = document.getElementById("category_id")
 const description = document.getElementById("description_id")
 const published =  document.getElementById("published_id")
+const cod_id = document.getElementById("cod_id")
 let option = ''
 
 btnCrear.addEventListener('click', () => {
+  cod_id.value= ""
   description.value = ""
   category.value = ""
   published.value = ""
@@ -18,61 +20,30 @@ btnCrear.addEventListener('click', () => {
   option = 'create'
 })
 
-
-
-// let filmsData = [
-//   {
-//     "id": "1",
-//     "nombre": "Pelicula 1",
-//     "categoria": "Acción",
-//     "descripcion": "Una emocionante película de acción",
-//     "publicado": 1
-//   },
-//   {
-//     "id": "2",
-//     "nombre": "Pelicula 2",
-//     "categoria": "Comedia",
-//     "descripcion": "Una divertida comedia para reír sin parar",
-//     "publicado": 1
-//   },
-//   {
-//     "id": "3",
-//     "nombre": "Pelicula 3",
-//     "categoria": "Drama",
-//     "descripcion": "Una película conmovedora que te hará reflexionar",
-//     "publicado": 0
-//   },
-//   {
-//     "id": "4",
-//     "nombre": "Pelicula 4",
-//     "categoria": "Drama",
-//     "descripcion": "Una película conmovedora que te hará reflexionar",
-//     "publicado": 0
-//   }
-// ];
-
-
-// localStorage.setItem("films", JSON.stringify(filmsData));
-showInfo(JSON.parse(localStorage.getItem("films")))
-
+if (JSON.parse(localStorage.getItem("films")) !== null) {
+  showInfo(JSON.parse(localStorage.getItem("films")))
+}
 
 function showInfo(films) {
   films.forEach(film => {
     result += `
-                <tr>
-                    <td id="id">${film.id}</td>
+                <tr data-film-I="${film.id}">
+                    <td id="id" >${film.id}</td>
                     <td id="name">${film.nombre}</td>
                     <td id="category">${film.categoria}</td>
                     <td id="description">${film.descripcion}</td>
-                    <td id="publicated">${film.publicado}</td>
+                    <td id="publicated">
+                    <i class="fa-regular ${film.publicado == 'Si' ? "fa-circle-check" :"fa-circle-xmark"}" id="buttons"></i>
+                    </td>
                     <td class="class-center">
-                      <a class="btnEdit btn btn-primary">
+                      <a class="btnEdit btn btn-primary" data-film-D="${film.descripcion}" data-film-C="${film.categoria}" data-film-id="${film.id}"
+                      data-film-P="${film.publicado}" data-film-N= "${film.nombre}">
                         <i class="fa-solid fa-pen-to-square"></i>
                       </a>
-                      <a class="btnDelete btn btn-danger">
+                      <a class="btnDelete btn btn-danger" data-film-id="${film.id}">
                         <i class="fa-solid fa-trash-can"></i>
                       </a>
-                      <a class="btnStar btn btn-warning">
+                      <a class="btnStar btn btn-warning" data-film-I="${film.id}" data-film-S="${film.star}">
                       <i class="fa-solid fa-star"></i>
                       </a>
                     </td>
@@ -82,7 +53,7 @@ function showInfo(films) {
     const btnDeleteList = container.querySelectorAll('.btnDelete');
     btnDeleteList.forEach(btnDelete => {
       btnDelete.addEventListener('click', () => {
-        const filmId = film.id;
+        const filmId = btnDelete.dataset.filmId;
         let films = JSON.parse(localStorage.getItem("films"));
         let updatedFilms = films.filter(film => film.id !== filmId);
         localStorage.setItem("films", JSON.stringify(updatedFilms));
@@ -92,13 +63,73 @@ function showInfo(films) {
     const btnEditList = container.querySelectorAll(".btnEdit");
     btnEditList.forEach(btnEdit => {
       btnEdit.addEventListener('click', () => {
-          description.value = ""
-          category.value = ""
-          published.value = ""
-          name_id.value = ""
+          description.value = btnEdit.dataset.filmD
+          category.value = btnEdit.dataset.filmC
+          published.value = btnEdit.dataset.filmP
+          name_id.value = btnEdit.dataset.filmN
+          cod_id.value = btnEdit.dataset.filmId
+          option = "edit"
           modaltableFilms.show()
-        
       })
+    })
+    let on = null
+    let first = true
+    const filmsStar = JSON.parse(localStorage.getItem('films'))
+    const filmStar = filmsStar.find(f => f.star == true)
+    const btnStarList = container.querySelectorAll(".btnStar");
+
+    if (filmStar) {
+      const tdElements = document.querySelectorAll(`tr[data-film-I="${filmStar.id}"] td`);
+          tdElements.forEach(tdElement => {
+            tdElement.classList.add("star");
+          });
+      console.log(btnStarList)
+      btnStarList.forEach(btnStar => {
+        if (btnStar.dataset.filmS == "true") on = btnStar
+      })
+          
+      first = false
+    }
+  
+    
+    btnStarList.forEach(btnStar => {
+      btnStar.addEventListener('click', () => {
+        if (on !== btnStar && first) { 
+          on = btnStar;
+          on.classList.add("selected");
+          const filmId = on.dataset.filmI;
+          const films = JSON.parse(localStorage.getItem('films'))
+          const tdElements = document.querySelectorAll(`tr[data-film-I="${filmId}"] td`);
+          tdElements.forEach(tdElement => {
+            tdElement.classList.add("star");
+          });
+          
+          const film = films.find(f => f.id == filmId)
+          if (film) {
+            film.star = true
+            localStorage.setItem('films', JSON.stringify(films))
+          }
+          first = false;
+          
+        } else {
+          console.log(on, btnStar)
+          if (on == btnStar) {
+            on.classList.remove("selected");
+            on = null;
+            const filmId = btnStar.dataset.filmI;
+            const tdElements = document.querySelectorAll(`tr[data-film-I="${filmId}"] td`);
+            tdElements.forEach(tdElement => {
+            tdElement.classList.remove("star"); })
+            const films = JSON.parse(localStorage.getItem('films'))
+            const film = films.find(f => f.id == filmId)
+            if (film) {
+              film.star = false
+              localStorage.setItem('films', JSON.stringify(films))
+            }
+            first = true
+          }
+        }}
+      )
     })
 
   });
@@ -123,7 +154,9 @@ function newFilm(data) {
     }
   } 
   else 
-  {
+  {   
+    console.log("entre")
+      console.log(data)
       arrayFilms.push(data);
       localStorage.setItem("films", JSON.stringify(arrayFilms));
   }
@@ -140,23 +173,42 @@ function checkFilms(id_film) {
 }
 
 
-
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault()
-  let cod_id = document.getElementById('cod_id').value
-  console.log(cod_id)
-  let name_id = document.getElementById('name_id').value
+  let cod_id_c = document.getElementById('cod_id').value
+  let name_id_c = document.getElementById('name_id').value
   let category_id = document.getElementById('category_id').value
   let description_id = document.getElementById('description_id').value
   let published_id = document.getElementById('published_id').value
-
-  const data = {
-    id : cod_id,
-    nombre: name_id,
-    categoria : category_id,
-    descripcion : description_id,
-    publicado : published_id
-  }
+  if (option == "create"){
+    const data = {
+      id : cod_id_c,
+      nombre: name_id_c,
+      categoria : category_id,
+      descripcion : description_id,
+      publicado : published_id,
+      star: false
+    }
   newFilm(data)
   location.reload();
+} else if (option == "edit") {
+    description.value = document.getElementById('description_id').value
+    category.value = document.getElementById('category_id').value
+    published.value = document.getElementById('published_id').value
+    name_id.value = document.getElementById('name_id').value
+    cod_id.value = document.getElementById('cod_id').value
+    let films = JSON.parse(localStorage.getItem("films"));
+    const update = films.map(film => {
+      if (cod_id.value == film.id) {
+        film.descripcion = description.value
+        film.publicado = published.value
+        film.nombre = name_id.value
+        film.categoria = category.value
+      }
+      return film
+    });
+    localStorage.setItem("films", JSON.stringify(update))
+    modaltableFilms.hide()
+    location.reload();
+}
 })
